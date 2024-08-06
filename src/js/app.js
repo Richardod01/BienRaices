0fb5e2763217db05ef61672e4aa34d9996075875
Richardod01 <j.ricardo@live.com.mx>
2024-08-06 09:28:22 -0600

    Avances en JS navegacion mobil y dark mode

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    eventListeners();
+});
+
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click',navegacionResponsive)
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+    navegacion.classList.toggle('mostrar');
+}
