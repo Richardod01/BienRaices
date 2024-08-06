863f1e1366ff81566b55425a4dfce6e09c1032cf
Richardod01 <j.ricardo@live.com.mx>
2024-08-06 10:50:20 -0600

    Finalizacion del proyecto html, darkmode etc.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     eventListeners();
+
+    darkMode();
 });
+
+function darkMode(){
+    const prefiereMode = window.matchMedia('(prefers-color-scheme: dark)')
+    // console.log(prefiereMode.matches) Comprobar si las preferencias del sistema oeprativo estan seteadas en dark mode o no
+    if (prefiereMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereMode.addEventListener('change', function(){
+        if (prefiereMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+
+    const botonDarkMode = document.querySelector(".dark-mode-boton")
+    botonDarkMode.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
